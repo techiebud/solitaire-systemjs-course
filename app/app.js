6c12765f38c7c8cd059a66bb0e85edcd252e3b38
Techiebud <techiebud@gmail.com>
2015-12-23 17:53:44 -0500

    Completed Chapter 2

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
-(function () {
-  "use strict";
+import "klondike/klondike.js";
 
-  angular.module("solitaire", ["klondike", "ngDraggable"]);
-})();
+angular.module("solitaire", ["klondike", "ngDraggable"]);
+angular.element(document).ready(() => {
+  angular.bootstrap(document, ["solitaire"]);
+});
